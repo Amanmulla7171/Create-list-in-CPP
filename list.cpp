847c0084598f9daa6b6f4ba31e36bb4262433921
Aman Mulla <163362618+Amanmulla7171@users.noreply.github.com>
2025-01-28 13:31:05 +0530

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+int main(){
+	string item[3]={"RICE","OIL","SOAP"};
+	int price[3]={50,1455,95};
+	int sum=0;
+	cout<<setw(5)<<left<<"Item"<<setw(7)<<right<<"Price"<<endl;
+	for(int i=0;i<3;i++){
+		
+		cout<<setw(5)<<left<<item[i]<<setw(5)<<right<<price[i]<<".00"<<endl;
+		sum+=price[i];
+	}
+	
+   cout<<"-----------------------"<<endl;
+	cout<<setw(5)<<left<<"total"<<setw(5)<<right<<sum<<".00";
+	return 0;
+}
+
